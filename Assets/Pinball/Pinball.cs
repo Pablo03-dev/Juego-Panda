52db4e410b340ac4eddc50879dcc2858badbb83d
Nicolás Santibañez <pablo.villegasa@alumnos.arcos.cl>
2022-06-22 10:03:53 -0400

    nueva ruta

---
+++ Assets/Pinball/Pinball.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinball : MonoBehaviour
+{
+    public float speed = 0f;
+    public HingeJoint2D hingeJoint2DLeft;
+    private JointMotor2D jointMotorLeft;
+
+    public HingeJoint2D hingeJoint2DRight;
+    private JointMotor2D jointMotorRight;
+
+    private void Start()
+    {
+        jointMotorLeft = hingeJoint2DLeft.motor;
+
+        jointMotorRight = hingeJoint2DRight.motor;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            jointMotorLeft.motorSpeed = -speed;
+            hingeJoint2DLeft.motor = jointMotorLeft;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            jointMotorLeft.motorSpeed = speed;
+            hingeJoint2DLeft.motor = jointMotorLeft;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            jointMotorRight.motorSpeed = speed;
+            hingeJoint2DRight.motor = jointMotorRight;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            jointMotorRight.motorSpeed = -speed;
+            hingeJoint2DRight.motor = jointMotorRight;
+        }
+    }
+}
