818cde36c7ec1bd83b8b42e1c5afbfa2bfdcc24d
Pablo03-dev <84738819+Pablo03-dev@users.noreply.github.com>
2022-06-24 17:52:46 -0400

    nada

--- Assets/Scripts/FlipperController.cs
+++ Assets/Scripts/FlipperController.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class FlipperController : MonoBehaviour
 {
-    public Animator LeftaflipperAnimator;
-    public Animator RightflipperAnimator;
+    public Animator PalancaAnimator;
+    
 
     //public Rigidbody2D left, right;
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if(Input.GetKeyDown(KeyCode.C))
         {
-            LeftaflipperAnimator.ResetTrigger("Pressed");
-            LeftaflipperAnimator.SetTrigger("Pressed");
+            PalancaAnimator.ResetTrigger("Pressed");
+            PalancaAnimator.SetTrigger("Pressed");
             //left.AddTorque(25f);
         }
-        else
-        {
-            //left.AddTorque(-20f);
-        }
+        //else
+        //{
+        //    //left.AddTorque(-20f);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            RightflipperAnimator.ResetTrigger("Pressed");
-            RightflipperAnimator.SetTrigger("Pressed");
-            //right.AddTorque(-25f);
-        }
-        else
-        {
-            //right.AddTorque(20f);
-        }
+        //if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    RightflipperAnimator.ResetTrigger("Pressed");
+        //    RightflipperAnimator.SetTrigger("Pressed");
+        //    //right.AddTorque(-25f);
+        //}
+        //else
+        //{
+        //    //right.AddTorque(20f);
+        //}
     }
 }
